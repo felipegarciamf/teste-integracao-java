7717e57c652f7ddd082f90aa6be7e6b3d9c62b79
felipe <silas.garcia@mfconsulting.com.br>
2020-04-07 23:42:00 -0300

    corringod mockito

--- test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
+++ test/br/com/caelum/pm73/dao/UsuarioDaoTest.java
@@ -14,28 +14,43 @@ import br.com.caelum.pm73.dominio.Usuario;
 
 public class UsuarioDaoTest {
 
-	private Session session;
 	private UsuarioDao usuarioDao;
+	private Session session;
 
 	@Before
 	public void antes() {
-		session = Mockito.mock(Session.class);
+		session = new CriadorDeSessao().getSession();
 		usuarioDao = new UsuarioDao(session);
+	}
+	
+	@After
+	public void depois() {
+		session.close();
 	}
 
 	@Test
 	public void deveEncontrarPeloNomeEmailMocado() {
+		Session session = Mockito.mock(Session.class);
 		Query query = Mockito.mock(Query.class);
 		UsuarioDao usuarioDao = new UsuarioDao(session);
+
 		Usuario usuario = new Usuario("Joao da Silva", "joaodasilva@teste.com");
+
 		String sql = "from Usuario u where u.nome = :nome and u.email = :email";
 		Mockito.when(session.createQuery(sql)).thenReturn(query);
 		Mockito.when(query.setParameter("nome", "Joao da Silva")).thenReturn(query);
 		Mockito.when(query.setParameter("email", "joaodasilva@teste.com")).thenReturn(query);
 		Mockito.when(query.uniqueResult()).thenReturn(usuario);
+
 		Usuario usuarioDoBanco = usuarioDao.porNomeEEmail("Joao da Silva", "joaodasilva@teste.com");
+
 		assertEquals(usuario.getNome(), usuarioDoBanco.getNome());
 		assertEquals(usuario.getEmail(), usuarioDoBanco.getEmail());
+	}
+
+	private void Mockito(Class<Session> class1) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Test
@@ -49,19 +64,18 @@ public class UsuarioDaoTest {
 		assertEquals("Joao da Silva", usuario.getNome());
 		assertEquals("joaodasilva@teste.com", usuario.getEmail());
 
+		
 	}
 
 	@Test
 	public void deveRetornarNuloSeNaoEncontrarUsuario() {
+		Session session = new CriadorDeSessao().getSession();
+		UsuarioDao usuarioDao = new UsuarioDao(session);
+
 		Usuario usuarioBanco = usuarioDao.porNomeEEmail("teste", "nao vai vir");
+
 		assertNull(usuarioBanco);
-	}
-
-	@After
-	public void depois() {
-		session.close();
-	}
 	
-
+	}
 
 }
